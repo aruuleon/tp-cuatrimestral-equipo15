3f1cf157c6f741ae78710888c595d4dfe86f2f98
Tomas Valero <tomasvalero22@gmail.com>
2024-06-13 20:57:06 -0300

    Creacion de Datos de prueba (TODOS)

--- Creacion_BBDD_Plataforma_Cursos.sql
+++ Creacion_BBDD_Plataforma_Cursos.sql
@@ -32,3 +32,28 @@ Create Table Usuarios_X_Cursos(
     primary key (IdCurso, IdUsuario),
 )
 
+
+---DATOS DE PRUEBA
+
+-- Insert data into Usuarios
+INSERT INTO Usuarios (IdMoodle, Nombre, Apellido, Email, Contrasenia, Avatar, Tipo) VALUES
+(101, 'Admin', 'Admin', 'admin@gmail.com', 'password123', 'avatar1.jpg', 1),
+(102, 'Maria', 'Gonzalez', 'maria.gonzalez@example.com', 'password456', 'avatar2.jpg', 0),
+(103, 'Carlos', 'Lopez', 'carlos.lopez@example.com', 'password789', 'avatar3.jpg', 0);
+GO
+
+-- Insert data into Cursos
+INSERT INTO Cursos (IdMoodle, Nombre, ImagenPortada, Descripcion, ConocimientosRequeridos, Programa, Precio, Visible) VALUES
+(301, 'C# Nivel 1', 'https://maxiprograma.com/assets/images/nivel-1.jpg', 'Este curso es el comienzo del código. Ideal para arrancar desde cero, complementar con el curso gratis u ordenar los conocimientos que tengas si ya venís intentando aprender. No requiere conocimientos previos.', 'Conocimientos básicos de manejo de PC. Contar con equipo de PC con acceso a Internet. No se requiere ningún conocimiento previo de programación.', 'https://drive.google.com/file/d/1O0LQTA7g-f9ZKo7U7alLqfJNK1cTEc71/view?pli=1', 30000, 1),
+(302, 'C# Nivel 2', 'https://maxiprograma.com/assets/images/nivel-2.jpg', 'Este curso es la continuación directa del Nivel 1, aunque también podés sumarte si ya contás con los fundamentos de programación en cualquier otro lenguaje. Acá comenzamos a construir apps vendibles.', 'Para poder aprovechar este curso al máximo es deseable que cuentes con una base de programación utilizando el lenguaje de programación C#, aunque si contás con conocimientos sólidos de las bases de programación con cualquier otro lenguaje y te interesa aprender uno nuevo, también estarás en condiciones de sumarte.', 'https://drive.google.com/file/d/1It68wtz-WqlGzKiWY9vwbI_0GgqfJew9/view', 30000, 1),
+(303, 'C# Nivel 3', 'https://maxiprograma.com/assets/images/nivel-3.jpg', 'Arrancamos con la etapa Web. En este curso tomamos todo el conocimiento que venimos trabajando y lo ponemos al servicio del desarrollo web sumando herramientas como HTML, CSS, JS y Bootstrap.', 'Para poder aprovechar este curso al máximo es necesario que cuentes con una base sólida en fundamentos de la programación, que conozcas el paradigma de Programación Orientada a Objetos y que al menos tengas nociones sobre .NET (o algún otro Framework). Los conocimientos necesarios los vemos en los Niveles 1 y 2, aunque vos podés tenerlos de otro lado, claro.', 'https://drive.google.com/file/d/16sTkD0qaHWPNmcjN9LqRN0fUG2PsitXz/view', 30000, 1);
+GO
+
+
+-- Insert data into Usuarios_X_Cursos
+INSERT INTO Usuarios_X_Cursos (IdCurso, IdUsuario) VALUES
+(1, 1),
+(2, 2),
+(3, 3);
+GO
+
