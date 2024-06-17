0c778ca680ac07e7a597b5079ff9add30015cd55
aruuleon <arieleonchang@gmail.com>
2024-06-17 20:58:05 -0300

    Creación de ventana AdministratorHome en donde los administradores van a poder visualizar un panel de control de toda la aplicación.

---
+++ tp-cuatrimestral-equipo15/AdministratorHome.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tp_cuatrimestral_equipo15 {
+    public partial class AdministratorHome : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Dictionary<string, string> SectionList = new Dictionary<string, string> {
+                { "Estudiantes", "fas fa-user-graduate" },
+                { "Cursos", "fas fa-book" },
+                { "Administradores", "fas fa-user-shield" },
+            };
+            sectionList.DataSource = SectionList;
+            sectionList.DataBind();
+        }
+    }
+}
